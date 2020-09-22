194eecc37f25b1262a1e5c99a7f336c1952158c8
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-09-22 23:31:55 +0300

    add FlatList

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, View, ScrollView } from 'react-native';
+import { StyleSheet, View, ScrollView, FlatList } from 'react-native';
 import { Navbar } from './src/Navbar';
 import { AddTodo } from './src/AddTodo';
 import { Todo } from './src/Todo'
@@ -10,6 +10,7 @@ export default function App() {
   const addTodo = (text) => {
     setTodos((prevTodos) => [{
       id: Date.now().toString(),
+      time: `${new Date().getHours()}:${new Date().getMinutes()}`,
       text
     }, ...prevTodos])
   }
@@ -17,11 +18,15 @@ export default function App() {
     <View style={styles.container} >
       <Navbar title='title of navbar' />
       <AddTodo addTodo={addTodo} />
-      <ScrollView>
+
+      <FlatList data={todos} keyExtractor={item => item.id}
+        renderItem={({item}) => <Todo text={item.text} time={item.time}/>}/>
+
+      {/* <ScrollView>
         {
-          todos.map((el, index) => <Todo key={index} text={el.text} />)
+          todos.map((el, index) => <Todo key={index} text={el.text} time={el.time}/>)
         }
-      </ScrollView>
+      </ScrollView> */}
       <StatusBar style="auto" />
     </View>
   );
--- src/Todo.js
+++ src/Todo.js
@@ -4,20 +4,32 @@ import {Text, View, StyleSheet} from 'react-native';
 export const Todo = (props) => {
     return (
         <View style={styles.todo}>
-            <Text>{props.text}</Text>
+            <View style={styles.textBlock}><Text style={styles.text}>{props.text}</Text></View>
+            <View><Text style={styles.time}>{props.time}</Text></View>
         </View>
     )
 }
 const styles = StyleSheet.create({
     todo: {
         flexDirection: 'row',
-        alignItems: 'center',
+        alignItems: 'flex-start',
         padding: 15,
         borderWidth: 1,
         borderColor: 'black',
         borderStyle: 'solid',
         borderRadius: 10,
         marginHorizontal: 10,
-        marginBottom: 10
+        marginBottom: 10,
+        justifyContent: 'space-between'
+    },
+    textBlock: {
+        textAlign: 'left',
+        width: '85%'
+    },
+    text: {
+        fontSize: 20
+    },
+    time: {
+        fontSize: 14
     }
 })
